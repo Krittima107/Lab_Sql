bceaf4799e675ce2271309d22d9482470636b06a
Mafuyuk <inverter186@gmail.com>
2025-08-21 14:04:13 +0700

    Add files via upload

--- LabSQL_21.sql
+++ LabSQL_21.sql
@@ -1,3 +1,35 @@
-select CategoryName,ProductName,UnitPrice
-from Products,Categories
-where products.CategoryID=Categories.CategoryID
+﻿--1 แสดงชื่อประเภทสินค้า ชื่อสินค้า และ ราคาสินค้า
+--CARTESIAN PRODUCT เชื่อมโยงที่ค าสั่ง WHERE
+Select CategoryName, ProductName, UnitPrice
+From Products as P, Categories as C
+where P.CategoryID=C.CategoryID
+and categoryName = 'seafood'
+
+--JOIN OPERATOR เชื่อมโยงที่คำสั่ง FROM...ON...
+Select CategoryName, ProductName,UnitPrice
+From Products as P Join Categories as C
+On P.CategoryID=C.CategoryID
+where categoryName = 'seafood'
+
+--จงแสดงข้อมูลหมายเลขใบสั่งซื้อ และชื่อบริษัท ขนส่งสินค้า
+Select CompanyName, OrderID
+FROM Orders, Shippers
+WHERE Shippers.ShipperID = Orders.Shipvia
+
+Select CompanyName, OrderID
+FROM Orders join Shippers
+ON Shippers.ShipperID = Orders.Shipvia
+
+--จงแสดงข้อมูลหมายเลขใบสั่งซื้อและชื่อบริษัทขนส่งสินค้าของใบสั่งซื้อหมายเลข 10275
+--Cartesian Product
+SELECT CompanyName, OrderID
+FROM Orders, Shippers
+WHERE Shippers.ShipperID = Orders.Shipvia
+AND OrderID = 10275
+
+--Join Operator
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID=Orders.Shipvia
+WHERE OrderID=10275
+
